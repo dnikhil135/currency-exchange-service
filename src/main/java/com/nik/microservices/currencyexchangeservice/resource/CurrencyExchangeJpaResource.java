3f429064d3115589aa6c1aa7e34ebc13765f5334
dnikhil135 <dnikhil11@gmail.com>
2018-05-06 10:03:18 +0530

    Add Rabbit MQ functionality

--- src/main/java/com/nik/microservices/currencyexchangeservice/resource/CurrencyExchangeJpaResource.java
+++ src/main/java/com/nik/microservices/currencyexchangeservice/resource/CurrencyExchangeJpaResource.java
@@ -1,5 +1,7 @@
 package com.nik.microservices.currencyexchangeservice.resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +20,14 @@ public class CurrencyExchangeJpaResource {
 	@Autowired
 	private ExchangeCurrencyRepository exchangeCurrencyRepository;
 	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@GetMapping("/currency-exchange-jpa/from/{from}/to/{to}")
 	public ExchangeCurrency retriveCurrencyValue(@PathVariable String from, @PathVariable String to) {
 		
 		ExchangeCurrency exchangeCurrency = exchangeCurrencyRepository.findByFromAndTo(from, to);
 		exchangeCurrency.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		logger.info("===> {}", exchangeCurrency);
 		return exchangeCurrency;
 	}
 }
